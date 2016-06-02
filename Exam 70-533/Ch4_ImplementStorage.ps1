0788be4641e27bdd7e030d69d55d9121334f6a2a
walimi <wahid.alimi@gmail.com>
2016-06-02 10:40:20 -0400

    Initial commit for Chapter 4: Implement storage

---
+++ Exam 70-533/Ch4_ImplementStorage.ps1
@@ -0,0 +1,7 @@
+﻿# Chapter 4: Implement Storage
+
+# Objective 4.1: Implement blobs and Azure files
+
+# To create a storage container
+New-AzureStorageContainer -Name "newcontainer" `
+                          -Permission Off  
